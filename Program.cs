d6c9c80210145a17c68e21edfe522d4e4c829ba1
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-27 13:12:31 +0530

    [Priyanshu]modified add details

--- Program.cs
+++ Program.cs
@@ -11,6 +11,28 @@ namespace LambdaExpression
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Lambda Expressions");
+            List<Person> list = new List<Person>();
+            IterateOverLoop(list);
+            AddingPerson(list);
         }
+        //UC1 Adding Details
+        public static void AddingPerson(List<Person> personList)
+        {
+            personList.Add(new Person() { SSN = 1, Age = 16, Name = "Shalu", Address = "Chennai" });
+            personList.Add(new Person() { SSN = 5, Age = 18, Name = "Shivam", Address = "Chennai" });
+            personList.Add(new Person() { SSN = 7, Age = 25, Name = "Shiva", Address = "Chennai" });
+            personList.Add(new Person() { SSN = 2, Age = 85, Name = "Ranu", Address = "Chennai" });
+            personList.Add(new Person() { SSN = 6, Age = 15, Name = "Ajay", Address = "Chennai" });
+            personList.Add(new Person() { SSN = 8, Age = 75, Name = "Nithya", Address = "Chennai" });
+        }
+
+        public static void IterateOverLoop(List<Person> personList)
+        {
+            foreach (Person person in personList)
+            {
+                Console.WriteLine("Age\t" + person.Age + "Name\t" + person.Name + "\t" + "Address\t" + person.Address);
+            }
+        }
+
     }
 }
